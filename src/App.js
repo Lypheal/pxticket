4abd821596fd665d0c760915dfa91046e9ab25f0
Bhavya <99541630+Lypheal@users.noreply.github.com>
2022-06-10 20:46:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -206,9 +206,6 @@ function App() {
         <StyledLogo alt={"logo"} src={"/config/images/logo.png"} />
         <s.SpacerSmall />
         <ResponsiveWrapper flex={1} style={{ padding: 24 }} test>
-         // <s.Container flex={1} jc={"center"} ai={"center"}>
-           // <StyledImg alt={"example"} src={"/config/images/example.gif"} />
-         // </s.Container>
           <s.SpacerLarge />
           <s.Container
             flex={2}
@@ -373,12 +370,6 @@ function App() {
           </s.Container>
           <s.SpacerLarge />
           <s.Container flex={1} jc={"center"} ai={"center"}>
-        //    <StyledImg
-          //    alt={"example"}
-           //   src={"/config/images/example.gif"}
-           //   style={{ transform: "scaleX(-1)" }}
-         //   />
-          </s.Container>
         </ResponsiveWrapper>
         <s.SpacerMedium />
         <s.Container jc={"center"} ai={"center"} style={{ width: "70%" }}>
